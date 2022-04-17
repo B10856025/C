1817ce174248380f6cbb7d7d7f33f78a131f9e13
B10856025 <64625184+B10856025@users.noreply.github.com>
2022-04-17 14:27:06 +0800

    Add files via upload

---
+++ 2.1 練習輸出與輸入-0302.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("See you tomorrow.\nHave a good night.\n") ;
+	printf("6+7+24=%d\n",6+7+24);
+	printf("*\n**\n***\n****\n*****");
+	return 0;
+}
