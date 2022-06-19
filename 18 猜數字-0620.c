d7dad23c9243641c9ea684949419863ca14cb129
B10856025 <64625184+B10856025@users.noreply.github.com>
2022-06-20 05:21:01 +0800

    Add files via upload
    猜數字
    
    一個人設定一組四碼的數字作為謎底，另一方猜。每猜一個數，出數者就要根據這個數字給出提示，提示以XAYB形式呈現，直到猜中為止。其中X表示位置正確的數的個數，而Y表示數字正確而位置不對的數的個數。
    
    不重複數字
    例如，當謎底為8123，而猜謎者猜1052時，出題者必須提示0A2B。
    
    例如，當謎底為5637，而猜謎者猜4931時，出題者必須提示1A0B。
    
    使用者按下q時 表示放棄，系統要將答案列出

---
+++ 18 猜數字-0620.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	int  guess, i, j, A=0, B=0;
+	int g[4];
+	int ans[4];
+	char ch = 'q';
+	char guess_ch;
+	srand(time(NULL));
+	printf("猜數字遊戲-輸入q結束遊戲\n");
+	for(i=0;i<4;i++){
+		do{
+			ans[i] = rand()%10;      //ans 取亂數存陣列 ; %9 : 0~8; %9+1 : 1~9
+			for(j=0;j<i;j++){
+				if(ans[i] == ans[j]){ //判斷是否有重複的值 是就跳出迴圈 
+					//printf("\n%d %d\n", ans[i], ans[j]);
+					break;
+				}
+			}
+		}while(i != j);            //i=j才離開 
+	}         
+	/*for(i=0;i<4;i++){          
+		printf("%d", ans[i]);     //印出答案
+	} */ 
+	while(A != 4){                    //當A不等於4 繼續猜 
+		printf("請輸入四個數字:\n"); 
+		scanf("%d", &guess);          //整數 
+		scanf("%c", &guess_ch);       //轉字元 
+		if(guess_ch == ch)  break;    //如果輸入Q跳出迴圈
+		g[0] = guess/1000;            //將猜的數字依序存到陣列 
+		g[1] = guess/100%10;
+		g[2] = guess/10%10;
+		g[3] = guess%10;
+		A=0, B=0;		
+		for(i=0;i<4;i++){
+			for(j=0;j<4;j++){
+				if((g[i] == ans[j]) && (i == j)){  //判斷i=j且位置相同 
+					A = A+1;
+				}		
+				if((g[i] == ans[j]) && (i != j)){  //判斷i=j但位置不相同
+					B = B+1; 
+				}		
+			}
+		}
+		printf("%dA%dB\n", A, B);	
+	}       
+	
+	if(A != 4){
+		printf("結束遊戲~\n正確答案 %d%d%d%d", ans[0], ans[1], ans[2], ans[3]);
+	}
+	else{
+		printf("\n恭喜答對!");
+	}
+	return 0;
+}
